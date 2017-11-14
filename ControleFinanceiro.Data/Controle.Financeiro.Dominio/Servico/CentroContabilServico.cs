c1089a85cf81f5cf507d6d6fa1df265b217643c9
VERITY\suelen.silva <suelen.silva@verity.com.br>
2017-11-14 18:33:47 -0200

    Alterações serviços controle financeiro

--- ControleFinanceiro.Data/Controle.Financeiro.Dominio/Servico/CentroContabilServico.cs
+++ ControleFinanceiro.Data/Controle.Financeiro.Dominio/Servico/CentroContabilServico.cs
@@ -10,8 +10,23 @@ namespace ControleFinanceiro.Dominio.Servico
 {
     public class CentroContabilServico : ServicoBase<CentroContabil>
     {
+        private DbFinancaContext _DbContexto;
+
         public CentroContabilServico(DbFinancaContext oDbContexto) : base(oDbContexto)
-        {   
+        {
+            _DbContexto = oDbContexto;
+        }
+
+        public List<CentroContabil> ObterPorDescricao (string descricao)
+        {
+            List<CentroContabil> oListaCentro = _DbContexto.CentroContabil.Where(o => o.Descricao.Contains(descricao)).ToList();
+            return oListaCentro;
+        }
+
+        public CentroContabil ObterPorCodigo(string codigo)
+        {
+            CentroContabil oCentroCusto = _DbContexto.CentroContabil.Where(o => o.Codigo.Equals(codigo)).First();
+            return oCentroCusto;
         }
     }
 }
--- ControleFinanceiro.Data/Controle.Financeiro.Dominio/Servico/ContaContabilServico.cs
+++ ControleFinanceiro.Data/Controle.Financeiro.Dominio/Servico/ContaContabilServico.cs
@@ -10,8 +10,25 @@ namespace ControleFinanceiro.Dominio.Servico
 {
     public class ContaContabilServico: ServicoBase<ContaContabil>
     {
+        private DbFinancaContext _DbContexto;
+
         public ContaContabilServico(DbFinancaContext oDbContexto): base(oDbContexto)
         {
+            _DbContexto = oDbContexto;
         }
+
+        public List<ContaContabil> ObterPorDescricao(string descricao)
+        {
+            List<ContaContabil> oListaContaContabil = _DbContexto.ContaContabil.Where(o => o.Descricao.Contains(descricao)).ToList();
+            return oListaContaContabil;
+        }
+
+        public ContaContabil ObterPorCodigo(string codigo)
+        {
+            ContaContabil oContaContabil = _DbContexto.ContaContabil.Where(o => o.Codigo.Equals(codigo)).First();
+            return oContaContabil;
+        }
+
     }
+
 }
