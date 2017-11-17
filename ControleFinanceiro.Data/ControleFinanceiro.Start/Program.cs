5b33f9334a51fcfb9a797dcbcc98698cae233b98
VERITY\suelen.silva <suelen.silva@verity.com.br>
2017-11-17 18:31:57 -0200

    Desenvolvimento - Aplicação Controle Financeiro

--- ControleFinanceiro.Data/ControleFinanceiro.Start/Program.cs
+++ ControleFinanceiro.Data/ControleFinanceiro.Start/Program.cs
@@ -9,6 +9,7 @@ using System.IO;
 using ControleFinanceiro.Entidade.Entidade;
 using ControleFinanceiro.Generics.Servicos;
 using ControleFinanceiro.Generics.Util;
+using System.Text.RegularExpressions;
 
 
 namespace ControleFinanceiro.Start
@@ -17,8 +18,75 @@ namespace ControleFinanceiro.Start
     {
         static void Main(string[] args)
         {
-            ConsultarPessoa1();
+            //ConsultarPessoa1();
+
+            //string campoAuxiliar = "RFQA;351881086765599;1;1.499,00;29,70";
+            string campoAuxiliar = "RFQA;358982060286835;10;1499,00;35,99";
+            Console.WriteLine(campoAuxiliar);
+            decimal valorPremio = ObtemValorProduto(campoAuxiliar);
+            Console.WriteLine(valorPremio);
+
+
+            campoAuxiliar = "RFQA;358982060286835;10";
+            Console.WriteLine(campoAuxiliar);
+            valorPremio = ObtemValorProduto(campoAuxiliar);
+            Console.WriteLine(valorPremio);
+
+            campoAuxiliar = "QA|358982060286835|10|2199,00|543,99";
+            Console.WriteLine(campoAuxiliar);
+            valorPremio = ObtemValorProduto(campoAuxiliar);
+            Console.WriteLine(valorPremio);
+            
             Console.ReadKey();
+        }
+
+
+        public static decimal ObtemValorProduto(string campoAuxiliar)
+        {
+            try
+            {
+                decimal valorPremio = 0M;
+
+                /* 1ª  Busca - Tenta identificar pelo separador | */
+                var objUnificado = campoAuxiliar.Split('|');
+                if (objUnificado != null)
+                {
+                    if (objUnificado.Length >= 4)
+                    {
+                        valorPremio = (string.IsNullOrWhiteSpace(objUnificado[4]) ? 0M : Convert.ToDecimal(objUnificado[4].ToString()));
+                    }
+                }
+                if (valorPremio > 0) return valorPremio;
+
+                /* 2ª  Busca - Tenta identificar pelo separador ; */
+                var objLegado = campoAuxiliar.Split(';');
+                if (objLegado != null)
+                {
+                    if (objLegado.Length >= 4)
+                    {
+                        valorPremio = (string.IsNullOrWhiteSpace(objLegado[4]) ? 0M : Convert.ToDecimal(objLegado[4].ToString()));
+                    }
+                }
+                if (valorPremio > 0) return valorPremio;
+
+
+                /* 3ª  Busca - Tenta identificar pelo separador ; via REGEX */
+                string[] aux = Regex.Split(campoAuxiliar, @";");
+                if (aux != null)
+                {
+                    if (aux.Length >= 4)
+                    {
+                        valorPremio = (string.IsNullOrWhiteSpace(aux[4]) ? 0M : Convert.ToDecimal(aux[4].ToString()));
+                    }
+                }
+
+                return valorPremio;
+            }
+            catch (Exception ex)
+            {
+                //Log.ErrorFormat("Erro no método ObtemValorProduto. Mensagem {1}.", ex.Message.ToString());
+                return 0M;
+            }
         }
 
         private static void CadastrarPessoaAsync()
